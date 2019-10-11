d47a3d25548bdbe4deb80ab7993dc5c5c4079969
Ryan Gonzalez <rymg19@gmail.com>
2019-10-10 19:49:13 -0500

    debugging

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -34,12 +34,13 @@ def run_cmd(cmd, stdin_data=None, expect=0, dontwritebytecode=False):
     p = subprocess.Popen(cmd,
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT,
+                         stderr=subprocess.PIPE,
                          universal_newlines=True,
                          shell=False,
                          env=env)
     output = p.communicate(input=stdin_data)
     assert p.wait() == expect
+    print(output[1])
     return output
 
 
